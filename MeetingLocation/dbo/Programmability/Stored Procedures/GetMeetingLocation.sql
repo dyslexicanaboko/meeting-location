6433d9cabe5bf031a945dabd6163d32bce88c3e0
Eli Hayon <dyslexicanaboko@gmail.com>
2020-07-20 00:34:59 -0400

    Modifications to the table to logically reduce the number of available options after one has been chosen. Changed how city is chosen randomly.

--- MeetingLocation/dbo/Programmability/Stored Procedures/GetMeetingLocation.sql
+++ MeetingLocation/dbo/Programmability/Stored Procedures/GetMeetingLocation.sql
@@ -1,23 +1,48 @@
 ﻿CREATE PROCEDURE [dbo].[GetMeetingLocation]
 AS
-	DECLARE @cities INT;
+BEGIN TRY
+	SET TRANSACTION ISOLATION LEVEL SNAPSHOT
+
+	BEGIN TRANSACTION
+		DECLARE @randId INT;
+		DECLARE @smallestUseCounter INT;
 	
-	SELECT @cities = COUNT(CityId) FROM dbo.City c
+		SELECT @smallestUseCounter = MIN(UseCounter) FROM dbo.City;
+		
+		SELECT TOP 1
+			@randId = c.CityId 
+		FROM dbo.City c 
+		WHERE c.UseCounter = @smallestUseCounter
+		ORDER BY NEWID()
+	
+		--SELECT @randId
+	
+		UPDATE dbo.City SET
+			 LastUsed = GETDATE()
+			,UseCounter = UseCounter + 1
+		WHERE CityId = @randId
+	
+		SELECT
+			 s.StateId
+			,s.Name AS StateName
+			,c.CityId
+			,c.Name AS CityName
+			,CONCAT(c.Name, ', ', s.Name) AS MeetingLocation
+		FROM dbo.State s
+			INNER JOIN dbo.City c
+				ON c.StateId = s.StateId
+		WHERE c.CityId = @randId
+	COMMIT TRANSACTION
+END TRY  
+BEGIN CATCH
+	ROLLBACK TRANSACTION
 
-	--SELECT @cities
-
-	DECLARE @randId INT = CEILING(RAND() * @cities)
-
-	--SELECT @randId
-
-	SELECT
-		 s.StateId
-		,s.Name AS StateName
-		,c.CityId
-		,c.Name AS CityName
-		,CONCAT(c.Name, ', ', s.Name) AS MeetingLocation
-	FROM dbo.State s
-		INNER JOIN dbo.City c
-			ON c.StateId = s.StateId
-	WHERE c.CityId = @randId
+	SELECT  
+		 ERROR_NUMBER() AS ErrorNumber  
+		,ERROR_SEVERITY() AS ErrorSeverity  
+		,ERROR_STATE() AS ErrorState  
+		,ERROR_PROCEDURE() AS ErrorProcedure  
+		,ERROR_LINE() AS ErrorLine  
+		,ERROR_MESSAGE() AS ErrorMessage;  
+END CATCH;
 RETURN 0
--- MeetingLocation/dbo/Tables/City.sql
+++ MeetingLocation/dbo/Tables/City.sql
@@ -2,6 +2,9 @@
 	[CityId] [INT] IDENTITY(1,1) NOT NULL,
 	[StateId] [INT] NOT NULL,
 	[Name] [VARCHAR](50) NOT NULL,
-	CONSTRAINT [PK_dbo.City_CityId] PRIMARY KEY CLUSTERED([CityId] ASC),
+	[UseCounter] INT CONSTRAINT [DF_dbo.City_UseCounter] DEFAULT (0) NOT NULL,
+	[LastUsed] DATETIME2(7) NULL,
+	[CreatedOn] DATETIME2(7) CONSTRAINT [DF_dbo.City_CreatedOn] DEFAULT (GETDATE()) NOT NULL,
+    CONSTRAINT [PK_dbo.City_CityId] PRIMARY KEY CLUSTERED([CityId] ASC),
 	CONSTRAINT [FK_dbo.City_dbo.State_StateId] FOREIGN KEY([StateId]) REFERENCES [dbo].[State] ([StateId])
 )
