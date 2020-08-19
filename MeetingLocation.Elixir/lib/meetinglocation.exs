ef7d556d0b4f917f53b5a9c9fc44352a89088981
Eli Hayon <dyslexicanaboko@gmail.com>
2020-08-19 00:58:56 -0400

    Finished making all changes. Script works as is however next round I want to clean it up into modules and files if possible.

--- MeetingLocation.Elixir/lib/meetinglocation.exs
+++ MeetingLocation.Elixir/lib/meetinglocation.exs
@@ -1,5 +1,24 @@
 #https://tech.findmypast.com/sql-server-in-elixir-connection/
 #https://github.com/findmypast-oss/mssqlex
+
+defmodule State do
+  defstruct [
+    StateId: 0,
+    Name: '',
+    CreatedOn: ~N[1900-01-01 00:00:00]]
+end
+
+defmodule City do
+  defstruct [
+    CityId: 0,
+    StateId: 0,
+    Name: '',
+    UseCounter: 0,
+    LastUsed: nil,
+    CreatedOn: ~N[1900-01-01 00:00:00],
+    State: nil]
+end
+
 defmodule Meetinglocation do
   def getMeetingLocation do
     #Get connection options that will fill the connection string
@@ -9,21 +28,33 @@ defmodule Meetinglocation do
     {_ok, pid} = connect(opts)
 
     IO.inspect(pid)
+    IO.puts("\n")
 
     #Get the smallest use count
     smallestUseCount = getSmallestUseCount(pid)
 
-    IO.puts("Smallest use counter: " <> to_string(smallestUseCount))
+    IO.puts("Smallest use counter: " <> to_string(smallestUseCount) <> "\n")
 
-    randomCity = getRandomCity(pid, smallestUseCounter)
+    randomCity = getRandomCity(pid, smallestUseCount)
 
     IO.inspect(randomCity)
 
-    #randomCity.UseCounter++;
-    #Update(randomCity);
-    #var meetingLocation = $"{randomCity.Name}, {randomCity.State.Name}";
+    #Incrementing the use and saving that changes
+    randomCity = incrementCityUseCount(pid, randomCity)
+
+    #Getting the state to get the name
+    state = getStateById(pid, randomCity."StateId")
+
+    IO.inspect(state)
+
+    #Updating the state reference
+    randomCity = %{randomCity | State: state}
+
+    strMeetingLocation = '#{randomCity."Name"}, #{randomCity."State"."Name"}'
 
     :odbc.stop()
+
+    strMeetingLocation
   end
 
   def getConnectionOptions() do
@@ -61,17 +92,47 @@ defmodule Meetinglocation do
 
     c = %City{}
 
-    [[{c.CityId, c.StateId, c.Name, c.UseCounter, c.LastUsed, c.CreatedOn}]] = rows
+    [[cityId, stateId, name, useCounter, lastUsed, createdOn]] = rows
+
+    c = %{c | CityId: cityId, StateId: stateId, Name: name, UseCounter: useCounter, LastUsed: lastUsed, CreatedOn: createdOn}
 
     c
   end
 
-  def getState do
-    IO.puts("Hello World")
+  def incrementCityUseCount(pid, %City{} = city) do
+    cityId = city."CityId"
+    useCounter = city."UseCounter" + 1
+    {_, lastUsed} = DateTime.now("Etc/UTC") #I don't care about timezone in this instance
+
+    #For now I am going to use cheap in-line SQL, at a later time I want to learn how to use Ecto that I keep reading about
+    statement = ~c(UPDATE dbo.City SET UseCounter = #{useCounter}, LastUsed = '#{lastUsed}' WHERE CityId = #{cityId})
+
+    IO.puts("#{statement}\n")
+
+    :odbc.sql_query(pid, statement)
+
+    city = %{city | UseCounter: useCounter, LastUsed: lastUsed}
+
+    city
   end
 
-  def incrementCityUseCount do
-    :world
+  def getStateById(pid, stateId) do
+    statement = 'SELECT TOP 1
+       s.StateId
+      ,s.Name
+      ,s.CreatedOn
+    FROM dbo.State s
+    WHERE s.StateId = #{stateId};'
+
+    {_selected, _colNames, rows} = :odbc.sql_query(pid, statement)
+
+    s = %State{}
+
+    [[stateId, name, createdOn]] = rows
+
+    s = %{s | StateId: stateId, Name: name, CreatedOn: createdOn}
+
+    s
   end
 
   def connect(opts) do
@@ -95,16 +156,5 @@ defmodule Meetinglocation do
     #IO.puts(conn_str)
     :odbc.start()
     :odbc.connect(conn_str, conn_opts)
-  end
-end
-
-defmodule City do
-  defstruct [
-    CityId: 0,
-    StateId: 0,
-    Name: "",
-    UseCounter: 0,
-    LastUsed: "" #DateTime.now("Etc/UTC")
-    CreatedOn: ""]
   end
 end
